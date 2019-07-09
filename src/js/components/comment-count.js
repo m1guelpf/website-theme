99f727d0ec8fce419c48e102da7145a5eb6b4f1d
Miguel Piedrafita <soy@miguelpiedrafita.com>
2019-07-09 17:03:26 +0200

    Only load when needed

--- src/js/components/comment-count.js
+++ src/js/components/comment-count.js
@@ -1,18 +1,20 @@
 import axios from 'axios'
 
-const paths = {}
-
-document.querySelectorAll('[data-comment-count]').forEach(el => {
-    paths[el.getAttribute('data-comments-for')] = el
-})
-
-axios.post('https://comments.m1guelpf.me/api/comment/count', {
-    domain: parent.location.host,
-    paths: Object.keys(paths)
-}, { headers: {'Content-type': 'application/x-www-form-urlencoded'}}).then(response => {
-    Object.entries(response.data.commentCounts).forEach(result => {
-        paths[result[0]].innerHTML = `&mdash; ${result[1]} ${result[1] == 1 ? 'comment':'comments'}`
+if (document.querySelector('[data-comment-count]')) {
+    const paths = {}
+    
+    document.querySelectorAll('[data-comment-count]').forEach(el => {
+        paths[el.getAttribute('data-comments-for')] = el
     })
-}).catch(error => {
-    console.log("[commento] error: " + error.message)
-})
+    
+    window.axios.post('https://comments.m1guelpf.me/api/comment/count', {
+        domain: parent.location.host,
+        paths: Object.keys(paths)
+    }, { headers: {'Content-type': 'application/x-www-form-urlencoded'}}).then(response => {
+        Object.entries(response.data.commentCounts).forEach(result => {
+            paths[result[0]].innerHTML = `&mdash; ${result[1]} ${result[1] == 1 ? 'comment':'comments'}`
+        })
+    }).catch(error => {
+        console.log("[commento] error: " + error.message)
+    })
+}
