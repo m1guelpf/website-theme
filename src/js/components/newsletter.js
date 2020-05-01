e1cdb1830cb1b55112e0a49c0e4357c733e45a7d
Miguel Piedrafita <github@miguelpiedrafita.com>
2020-05-01 03:45:17 +0200

    Update error handling

--- src/js/components/newsletter.js
+++ src/js/components/newsletter.js
@@ -19,20 +19,16 @@ if (newsletterForm && emailField && newsletterSuccess) {
         axios.post('https://newsletter.m1guelpf.me/api/subscribe/8d2265d5-111d-41b2-b170-11d4aff0dca7', {
             email: emailField.value,
         }).then(response => {
-            if (response.data.success) {
-                newsletterForm.style.display = 'none'
-                newsletterSuccess.style.display = null
-                
-                fathom('trackGoal', 'Z8TWLFZX', 0);
+            newsletterForm.style.display = 'none'
+            newsletterSuccess.style.display = null
 
-                try {
-                    localStorage.setItem('subscribedToNewsletter', true);
-                } catch (err) {}
-            } else {
-                alert(response.data.message)
-            }
-        }).catch(() => {
-            alert('Something went wrong. Please try again or DM @m1guelpf on Twitter :)')
+            fathom('trackGoal', 'Z8TWLFZX', 0);
+
+            try {
+                localStorage.setItem('subscribedToNewsletter', true);
+            } catch (err) {}
+        }).catch(error => {
+            alert(error.response.data.message)
         })
     })
 }
