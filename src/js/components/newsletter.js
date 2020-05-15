8495811116e21c49a01722c8708183ca82eb94d1
Miguel Piedrafita <soy@miguelpiedrafita.com>
2020-05-15 13:33:18 +0200

    Fix newsletter when already subscribed

--- src/js/components/newsletter.js
+++ src/js/components/newsletter.js
@@ -9,17 +9,17 @@ window.newsletter = () => ({
 	submitForm() {
 		axios.post('https://newsletter.m1guelpf.me/api/subscribe/8d2265d5-111d-41b2-b170-11d4aff0dca7', {
 			email: this.email,
-		}).then(() => {
-			this.subscribed = true
+		}).then(() => this.subscriptionSucceeded()).catch(error => {
+			if (error.response && error.response.data.code == 'already_subscribed') return this.subscriptionSucceeded()
 
-			fathom('trackGoal', 'Z8TWLFZX', 0);
-
-			this.persistSubscription()
-		}).catch(error => {
 			alert(error.response.data.message)
 		})
 	},
-	persistSubscription() {
+	subscriptionSucceeded() {
+		this.subscribed = true
+
+		fathom('trackGoal', 'Z8TWLFZX', 0);
+
 		localStorage.setItem('subscribedToNewsletter', true);
-	}
+	},
 })
